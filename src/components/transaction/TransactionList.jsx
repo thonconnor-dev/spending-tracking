2511862162e3d389caf3e37f3d2dd8369ce9bb9b
Sethon <thonconnor.dev@gmail.com>
2025-03-21 22:08:05 -0700

    add context state for income data (#5)
    * implement event submit new income
    
    * fix eslint error prop-type on styled component
    
    * add context state for income data
    
    * add context state for income data
    
    ---------
    
    Co-authored-by: Sethon <sethon@Sethons-MacBook-Pro.local>

--- src/components/transaction/TransactionList.jsx
+++ src/components/transaction/TransactionList.jsx
@@ -8,7 +8,7 @@ import PropTypes from "prop-types";
 import {TopContainer} from "../../styles/ListStyled.js";
 import {PieChart} from "@mui/x-charts";
 
-function TransactionList({ currentTime}) {
+function TransactionList({currentTime}) {
     const rows = [
         {
             id: "1",
@@ -41,37 +41,39 @@ function TransactionList({ currentTime}) {
     ];
     const [openDialog, setOpenDialog] = useState(false);
 
-
-    // useEffect(() => {
-    //   grow.current = true;
-    // },[location])
     return (
         <Grow in={true}>
             <Box
                 sx={{
                     backgroundColor: "white",
-                    marginTop: "10px",
-                    marginLeft: "40px",
-                    marginRight: "20%",
                     padding: "20px",
                     boxShadow: "0px 0px 10px 5px #b0b4b4",
-                    borderRadius: "5px"
+                    borderRadius: "5px",
+                    width: "60%",
                 }}
                 gap={1}
             >
-                <h1>Transaction</h1>
-                <FormControl sx={{width: "60%"}}>
-                    <OutlinedInput
-                        placeholder="Search Transaction"
-                        endAdornment={<Search/>} ß
-                    />
-                </FormControl>
+                <h2>{currentTime}</h2>
+                <PieChart
+                    series={[
+                        {
+                            data: [
+                                {id: 0, value: 10, label: 'Gas'},
+                                {id: 1, value: 15, label: 'Grocery'},
+                                {id: 2, value: 20, label: 'Utilities'},
+                            ],
+                        },
+                    ]}
+                    width={400}
+                    height={200}
+                />
                 <TopContainer>
-                    <h2>{currentTime}</h2>
-                    <ExpenseDialog
-                        openDialog={openDialog}
-                        setOpenDialogCallback={setOpenDialog}
-                    />
+                    <FormControl sx={{width: "60%"}}>
+                        <OutlinedInput
+                            placeholder="Search Transaction"
+                            endAdornment={<Search/>}
+                        />
+                    </FormControl>
                     <Button
                         variant="contained"
                         size="md"
@@ -82,20 +84,11 @@ function TransactionList({ currentTime}) {
                         New Expense
                     </Button>
                 </TopContainer>
-                <PieChart
-                    series={[
-                        {
-                            data: [
-                                { id: 0, value: 10, label: 'Gas' },
-                                { id: 1, value: 15, label: 'Grocery' },
-                                { id: 2, value: 20, label: 'Utilities' },
-                            ],
-                        },
-                    ]}
-                    width={400}
-                    height={200}
+                <TransactionItem items={rows} showNoOfItem={5}/>
+                <ExpenseDialog
+                    openDialog={openDialog}
+                    setOpenDialogCallback={setOpenDialog}
                 />
-                <TransactionItem items={rows}/>
             </Box>
         </Grow>
     );
